018e960e488f04863317dcc305fbf0f5679fefd2
Lee-Dongwook <dlehddnr0713@gmail.com>
2025-05-06 16:23:05 +0900

    feat : error page

---
+++ src/pages/error/404.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import Layout from "./components/Layout.vue";
+import Svg404 from "./images/404.svg?component";
+</script>
+
+<template>
+  <Layout>
+    <Svg404 />
+  </Layout>
+</template>
---
+++ src/pages/login/apis/index.ts
@@ -0,0 +1,17 @@
+import type * as Auth from "./type";
+import { request } from "@/http/axios";
+
+export function getCaptchaApi() {
+  return request<Auth.CaptchaResponseData>({
+    url: "auth/captcha",
+    method: "get",
+  });
+}
+
+export function loginApi(data: Auth.LoginRequestData) {
+  return request<Auth.LoginResponseData>({
+    url: "auth/login",
+    method: "post",
+    data,
+  });
+}
